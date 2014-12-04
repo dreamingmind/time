68684847a7673e1f97402b032633235a22eb78c9
Don Drake <ddrake@dreamingmind.com>
2014-12-04 15:03:07 -0800

    bake new Task model

---
+++ app/Test/Case/Controller/TasksControllerTest.php
@@ -0,0 +1,64 @@
+<?php
+App::uses('TasksController', 'Controller');
+
+/**
+ * TasksController Test Case
+ *
+ */
+class TasksControllerTest extends ControllerTestCase {
+
+/**
+ * Fixtures
+ *
+ * @var array
+ */
+	public $fixtures = array(
+		'app.task',
+		'app.project',
+		'app.client',
+		'app.time',
+		'app.user',
+		'app.group'
+	);
+
+/**
+ * testIndex method
+ *
+ * @return void
+ */
+	public function testIndex() {
+	}
+
+/**
+ * testView method
+ *
+ * @return void
+ */
+	public function testView() {
+	}
+
+/**
+ * testAdd method
+ *
+ * @return void
+ */
+	public function testAdd() {
+	}
+
+/**
+ * testEdit method
+ *
+ * @return void
+ */
+	public function testEdit() {
+	}
+
+/**
+ * testDelete method
+ *
+ * @return void
+ */
+	public function testDelete() {
+	}
+
+}
---
+++ app/Test/Case/Model/TaskTest.php
@@ -0,0 +1,45 @@
+<?php
+App::uses('Task', 'Model');
+
+/**
+ * Task Test Case
+ *
+ */
+class TaskTest extends CakeTestCase {
+
+/**
+ * Fixtures
+ *
+ * @var array
+ */
+	public $fixtures = array(
+		'app.task',
+		'app.project',
+		'app.client',
+		'app.time',
+		'app.user',
+		'app.group'
+	);
+
+/**
+ * setUp method
+ *
+ * @return void
+ */
+	public function setUp() {
+		parent::setUp();
+		$this->Task = ClassRegistry::init('Task');
+	}
+
+/**
+ * tearDown method
+ *
+ * @return void
+ */
+	public function tearDown() {
+		unset($this->Task);
+
+		parent::tearDown();
+	}
+
+}
