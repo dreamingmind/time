b03946097538b3f21ac7180bd51914b32538943b
Don Drake <ddrake@dreamingmind.com>
2014-12-29 21:16:40 -0800

    Add comments to the new Report component

--- app/Controller/Component/ReportComponent.php
+++ app/Controller/Component/ReportComponent.php
@@ -25,14 +25,56 @@ class ReportComponent extends Component {
 	 */
 	private $time;
 	
+	/**
+	 * Total time for users given current found Time records
+	 *
+	 * @var array
+	 */
 	private $userCumm;
-		
+	
+	/**
+	 * Project time breakdown given current found Time records
+	 * 
+	 * project A
+	 *	Time : total
+	 *	Users
+	 *		user a
+	 *			time : total
+	 *		user b
+	 *			time : total
+	 *	Tasks
+	 *		task a
+	 *			time : total
+	 *			user a : time total
+	 *			user b : time total
+	 * project B
+	 *	.
+	 *	.
+	 *	.
+	 *
+	 * @var array
+	 */
 	private $projectCumm;
 	
+	/**
+	 * Lookup list to convert project IDs to names
+	 *
+	 * @var array
+	 */
 	private $projects;
 	
+	/**
+	 * Lookup list to convert user IDs to names
+	 *
+	 * @var array
+	 */
 	private $users;
 	
+	/**
+	 * Lookup list to convert task IDs to names
+	 *
+	 * @var array
+	 */
 	private $tasks;
 
 	public function initialize(Controller $controller) {
@@ -55,6 +97,22 @@ class ReportComponent extends Component {
 		
 	}
 
+	/**
+	 * Summarize the times for this set of time records
+	 * 
+	 * Numerically indexed set of time records. 
+	 * 0 => array(
+	 *	field => value
+	 *	field => value
+	 * 1 => array(
+	 *	.
+	 *	.
+	 *	.
+	 * 
+	 * sets userCumm and projectCumm
+	 * 
+	 * @param array $timeEntries
+	 */
 	public function summarize($timeEntries) {
 		// build id=>name lookup properties;
 		$this->initProperties();
@@ -96,6 +154,11 @@ class ReportComponent extends Component {
 		}
 	}
 
+	/**
+	 * Make the ID -> name lookup lists
+	 * 
+	 * These are used to make meaningful indexes in the time summary arrays
+	 */
 	private function initProperties(){
 		$project = ClassRegistry::init('Project');
 		$user = ClassRegistry::init('User');
@@ -105,6 +168,13 @@ class ReportComponent extends Component {
 		$this->tasks = $task->find('list');
 	}
 
+	/**
+	 * Accummulate total time for a user
+	 * 
+	 * Works from this->time, saves to this->userCumm
+	 * 
+	 * @param float $duration
+	 */
 	private function userCumm($duration){
 		if (!isset($this->userCumm[ $this->userName() ])) {
 			$this->userCumm[ $this->userName() ] = 0;
@@ -112,6 +182,13 @@ class ReportComponent extends Component {
 		$this->userCumm[ $this->userName() ] += $duration;
 	}
 	
+	/**
+	 * Accummulate total time for a project
+	 * 
+	 * Works from this->time, saves to this->projectCumm
+	 * 
+	 * @param float $duration
+	 */
 	private function projectCumm($duration) {
 		if (!isset($this->projectCumm['Project'][ $this->projectName() ])) {
 			$this->projectCumm['Project'][ $this->projectName() ]['Time'] = 0;
@@ -119,6 +196,13 @@ class ReportComponent extends Component {
 		$this->projectCumm['Project'][ $this->projectName() ]['Time'] += $duration;
 	}
 	
+	/**
+	 * Accummulate total time for a projects task
+	 * 
+	 * Works from this->time, saves to this->projectCumm
+	 * 
+	 * @param float $duration
+	 */
 	private function taskCumm($duration){
 		if (!isset($this->projectCumm['Project'][ $this->projectName() ]['Task'][ $this->taskName() ])) {
 			$this->projectCumm['Project'][ $this->projectName() ]['Task'][ $this->taskName() ]['Time'] = 0;
@@ -126,6 +210,13 @@ class ReportComponent extends Component {
 		$this->projectCumm['Project'][ $this->projectName() ]['Task'][ $this->taskName() ]['Time'] += $duration;
 	}
 
+	/**
+	 * Accummulate total time for a user on a specific project/task
+	 * 
+	 * Works from this->time, saves to this->projectCumm
+	 * 
+	 * @param float $duration
+	 */
 	private function taskUserCumm($duration) {
 		if (!isset($this->projectCumm['Project'][ $this->projectName() ]['Task'][ $this->taskName() ][ $this->userName() ])) {
 			$this->projectCumm['Project'][ $this->projectName() ]['Task'][ $this->taskName() ][ $this->userName() ] = 0;
@@ -133,6 +224,13 @@ class ReportComponent extends Component {
 		$this->projectCumm['Project'][ $this->projectName() ]['Task'][ $this->taskName() ][ $this->userName() ] += $duration;
 	}
 	
+	/**
+	 * Accummulate total time for a user on a project
+	 * 
+	 * Works from this->time, saves to this->projectCumm
+	 * 
+	 * @param float $duration
+	 */
 	private function projectUserCumm($duration) {
 		if (!isset($this->projectCumm['Project'][ $this->projectName() ]['User'][ $this->userName() ]['Time'])) {
 			$this->projectCumm['Project'][ $this->projectName() ]['User'][ $this->userName() ]['Time'] = 0;
@@ -140,16 +238,33 @@ class ReportComponent extends Component {
 		$this->projectCumm['Project'][ $this->projectName() ]['User'][ $this->userName() ]['Time'] += $duration;
 	}
 	
+	/**
+	 * Calculate the duration (in hours) of the current this->time
+	 * 
+	 * Trims to 2 decimal places so there will be slight rounding errors
+	 * 
+	 * @return float
+	 */
 	private function duration() {
 		$dur = explode(':', $this->time['duration']);
 //		return (($dur[0] * HOUR) + ($dur[1] * MINUTE) + $dur[2]) / HOUR;
 		return number_format((($dur[0] * HOUR) + ($dur[1] * MINUTE) + $dur[2]) / HOUR , 2);
 	}
 	
+	/**
+	 * Return the name of the user linked to this->time
+	 * 
+	 * @return string
+	 */
 	private function userName() {
 		return  $this->users[$this->time['user_id']];
 	}
 
+	/**
+	 * Return the name of the task linked to this->time
+	 * 
+	 * @return string
+	 */
 	private function taskName() {
 		if (is_null($this->time['task_id'])) {
 			return ('un-named');
@@ -157,6 +272,11 @@ class ReportComponent extends Component {
 		return  $this->tasks[$this->time['task_id']];
 	}
 
+	/**
+	 * Return the name of the project linked to this->time
+	 * 
+	 * @return string
+	 */
 	private function projectName() {
 		return  $this->projects[$this->time['project_id']];
 	}
